d9d5625a81d6faa33b1299997328684e06ef27bb
awsp <anthonyspwu@gmail.com>
2015-12-11 09:37:41 -1000

    fix a typo in server.sh

--- server.sh
+++ server.sh
@@ -10,7 +10,7 @@ echo "Using Chrome Driver: $defaultDriver"
 chromeDriver="./drivers/chromedriver_$defaultDriver"
 if [ -f "$chromeDriver" ]
 then
-    java -jar bin/selenium-server-standalone-2.46.0.jar -Dwebdriver.chrome.driver="./drivers/$chromeDriver"
+    java -jar bin/selenium-server-standalone-2.46.0.jar -Dwebdriver.chrome.driver="$chromeDriver"
 else
     echo "$chromeDriver not found. Available options are [win32, mac32, linux32, linux64]"
 fi
