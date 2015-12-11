da1b91c6bce6a073cec4f3f7a768e3b09ea2ae87
awsp <anthonyspwu@gmail.com>
2015-12-11 09:02:31 -1000

    add more options for other platform, improve shell script

--- server.sh
+++ server.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+defaultDriver="mac32"
+if [ "$#" -gt 0 ]
+then
+    defaultDriver=$1
+fi
 
-java -jar bin/selenium-server-standalone-2.46.0.jar -Dwebdriver.chrome.driver="./drivers/chromedriver"
+echo "Using Chrome Driver: $defaultDriver"
+
+chromeDriver="./drivers/chromedriver_$defaultDriver"
+if [ -f "$chromeDriver" ]
+then
+    java -jar bin/selenium-server-standalone-2.46.0.jar -Dwebdriver.chrome.driver="./drivers/$chromeDriver"
+else
+    echo "$chromeDriver not found. Available options are [win32, mac32, linux32, linux64]"
+fi
